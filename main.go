2384a8bfe75d89c63d1d8a0881dc6f02f32f6fc0
Planutim <danceontheair@gmail.com>
2021-09-30 02:41:10 +0600

    added thread lock

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"math"
+	"runtime"
 
 	"github.com/Planutim/myopengl"
 	"github.com/go-gl/gl/v4.1-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
+func init() {
+	runtime.LockOSThread()
+}
 func main() {
 	window := myopengl.InitGlfw(800, 600)
 	window.SetKeyCallback(keyCallback)
